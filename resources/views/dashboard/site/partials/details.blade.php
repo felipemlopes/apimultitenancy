7fc25e6f3841861b9d2adc51f4acf40df1aa4c8d
Felipe Lopes <felipemarcanthlopes@gmail.com>
2025-01-03 11:16:07 -0300

    ajustes

--- resources/views/dashboard/site/partials/details.blade.php
+++ resources/views/dashboard/site/partials/details.blade.php
@@ -55,7 +55,7 @@
 
 <div class="mb-4">
     <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-        DB host
+        DB port
     </label>
     <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="db_port" name="db_port" type="text" value="{{($edit and $site)?$site->db_port:old('db_port')}}">
     @error('db_port')
