3b378ecac4f0d4a88cca7617d64848b56a52a5c7
felipemlopes <felipemarcanthlopes@gmail.com>
2025-03-03 16:49:32 -0300

    ajustes

--- app/Http/Controllers/queue/OrdersController.php
+++ app/Http/Controllers/queue/OrdersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Jobs\AprovePayment;
 use App\Jobs\PlaceOrder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrdersController extends Controller
 {
